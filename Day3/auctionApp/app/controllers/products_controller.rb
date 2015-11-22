ad8f4b7648e933baa2d0cce6f62db0e6d1758aac
Roberto Quesada <robertoquesadalizana@gmail.com>
2015-11-22 13:15:46 +0100

    Add views show, new and index for products model

--- Day3/auctionApp/app/controllers/products_controller.rb
+++ Day3/auctionApp/app/controllers/products_controller.rb
@@ -3,4 +3,32 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @product = @user.products.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @product = @user.products.new(product_params)
+
+    if @product.save
+      redirect_to('/products')
+    else
+      render 'new'
+    end
+
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :deadline)
+  end
+
 end
--- Day3/auctionApp/app/models/product.rb
+++ Day3/auctionApp/app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   belongs_to :user
+
+  validates :title, :description, :deadline, presence: true
 end
